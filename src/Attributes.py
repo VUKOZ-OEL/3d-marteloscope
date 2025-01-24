2785307e3320312486d6a2c8c89e26462e788438
m-a-r-t-in <krucek.martin@gmail.com>
2025-01-24 13:06:13 +0100

    Update Attributes.py

--- src/Attributes.py
+++ src/Attributes.py
@@ -4,10 +4,11 @@ import json
 from st_aggrid import AgGrid, GridOptionsBuilder, DataReturnMode, GridUpdateMode
 from src.io import *
 
-# Načtení dat do session_state
+
 if "trees" not in st.session_state:
     file_path = "c:/Users/krucek/OneDrive - vukoz.cz/DATA/_GS-LCR/LS-Krivoklat/3df_project/Krivoklat.json"
     st.session_state.trees = load_project_json(file_path)
+
 
 col1, col2, col3 = st.columns(3)
 with col1:
@@ -22,85 +23,7 @@ gb = GridOptionsBuilder.from_dataframe(st.session_state.trees)
 gb.configure_selection("multiple", use_checkbox=True)  # Povolit výběr více řádků
 gb.configure_grid_options(domLayout="normal")  # Standardní rozložení
 gb.configure_columns(["status"], editable=True)
-grid_options = gb.build()
-
-# Zobrazení interaktivní tabulky
-response = AgGrid(
-    st.session_state.trees,
-    gridOptions=grid_options,
-    update_mode=GridUpdateMode.VALUE_CHANGED,  # Změny hodnot v reálném čase
-    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-    theme="streamlit",
-    enable_enterprise_modules=False,
-    fit_columns_on_grid_load=True,
-)
-
-# Získání vybraných řádků
-selected_rows = response.get("selected_rows", [])
-
-# Zpracování tlačítek
-if isinstance(selected_rows, list) and len(selected_rows) > 0:  # Ověření, že jsou řádky vybrány
-    selected_ids = [row["id"] for row in selected_rows]  # Extrahujeme ID vybraných řádků
-
-    if target_tree_btn:
-        st.session_state.trees.loc[
-            st.session_state.trees["id"].isin(selected_ids), "status"
-        ] = "Target tree"
-        st.success(f"Status byl aktualizován na 'Target tree' pro ID: {selected_ids}")
-    elif remove_btn:
-        st.session_state.trees.loc[
-            st.session_state.trees["id"].isin(selected_ids), "status"
-        ] = "Remove"
-        st.success(f"Status byl aktualizován na 'Remove' pro ID: {selected_ids}")
-    elif unselect_btn:
-        st.session_state.trees.loc[
-            st.session_state.trees["id"].isin(selected_ids), "status"
-        ] = "Unselect"
-        st.success(f"Status byl aktualizován na 'Unselect' pro ID: {selected_ids}")
-
-    # Reset výběru (znovu vykreslení bez výběru)
-    response["selected_rows"] = []
-    gb = GridOptionsBuilder.from_dataframe(st.session_state.trees)
-    gb.configure_selection("multiple", use_checkbox=True)
-    grid_options = gb.build()
-
-    # Aktualizujeme tabulku
-    AgGrid(
-        st.session_state.trees,
-        gridOptions=grid_options,
-        update_mode=GridUpdateMode.NO_UPDATE,  # Žádné přímé změny uživatelského výběru
-        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-        theme="streamlit",
-        enable_enterprise_modules=False,
-        fit_columns_on_grid_load=True,
-    )
-else:
-    st.warning("Žádné řádky nebyly vybrány.")
-
-""" import streamlit as st
-import pandas as pd
-import json
-from st_aggrid import AgGrid, GridOptionsBuilder, DataReturnMode, GridUpdateMode
-from src.io import *
-
-if "trees" not in st.session_state:
-    file_path = "c:/Users/krucek/OneDrive - vukoz.cz/DATA/_GS-LCR/LS-Krivoklat/3df_project/Krivoklat.json"
-    st.session_state.trees = load_project_json(file_path)
-
-
-col1, col2, col3 = st.columns(3)
-with col1:
-    target_tree_btn = st.button("Target tree")
-with col2:
-    remove_btn = st.button("Remove")
-with col3:
-    unselect_btn = st.button("Unselect")
-
-# Konfigurace tabulky
-gb = GridOptionsBuilder.from_dataframe(st.session_state.trees)
-gb.configure_selection("multiple", use_checkbox=True)  # Povolit výběr více řádků
-gb.configure_grid_options(domLayout="normal")  # Standardní rozložení
-gb.configure_columns(["status"], editable=True) 
+gb.configure_side_bar(filters_panel=True, defaultToolPanel='filters')
 grid_options = gb.build()
 
 # Zobrazení interaktivní tabulky
@@ -138,4 +61,5 @@ if selected_rows is not None and not selected_rows.empty:  # Zkontrolujeme, zda 
             st.session_state.trees["id"].isin(selected_ids), "status"
         ] = "Unselect"
         st.success(f"Status byl aktualizován na 'Unselect' pro ID: {selected_ids}")
- """
+
+response.set
