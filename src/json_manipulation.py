7d8441a592231f7f127c7fbb963aa88184af8020
Martin Krůček <krucek@vukoz.cz>
2025-06-03 12:31:01 +0200

    Update json_manipulation.py

--- src/json_manipulation.py
+++ src/json_manipulation.py
@@ -12,9 +12,13 @@ print(dir(spp))
 file_path = "c:/Users/krucek/OneDrive - vukoz.cz/DATA/_GS-LCR/LS-Krivoklat/3df_project/Krivoklat_test_SAVE.json"
 trees = io.load_project_json(file_path)
 
-trees["management_status"] = "untouched"
-trees["species"] = trees["label"].apply(spp.map_species_codes2latin)
-trees["dbh"] = trees["dbh"].where(trees["dbh"] < 200, 0)
+#trees["management_status"] = "untouched"
+#trees["species"] = trees["label"].apply(spp.map_species_codes2latin)
+#trees["dbh"] = trees["dbh"].where(trees["dbh"] < 200, 0)
+
+trees["surfaceAreaProjection"] = 0
+trees["surfaceArea"] = 0
+trees["volume"] = 0
 
 io.save_project_json(file_path,trees,"c:/Users/krucek/OneDrive - vukoz.cz/DATA/_GS-LCR/LS-Krivoklat/3df_project/Krivoklat_test_SAVE.json")
 
