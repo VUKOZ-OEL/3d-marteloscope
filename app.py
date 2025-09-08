bc6ceb7e8eae6e54ed24e44909e091edd66a1915
Martin Krůček <krucek@vukoz.cz>
2025-09-08 17:18:37 +0200

    add requirements.txt

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import streamlit as st
 import src.io_utils as iou
 
-#from streamlit_multipage import MultiPage
 
+# Set Page Title
 st.set_page_config(page_title="3D-Marteloscope", page_icon=":material/nature_people:",layout="wide")
 
+# Dash page with summary
 dash_page = st.Page("src/Dashboard.py", title="Plot Info", icon=":material/dashboard:")
 
+# Tree & Crown stats
 tree_page = st.Page("src/Tree_stats.py", title="Tree Statistics", icon=":material/monitoring:")
 canopy_page = st.Page("src/Canopy_stats.py", title="Canopy Space", icon=":material/forest:")
 
+# Map, Att tab, pygpage
 map_page = st.Page("src/Map.py", title="Plot Map", icon=":material/map:")
 att_page = st.Page("src/Attributes.py", title="Attribute Table", icon=":material/data_table:")
 analytics_page = st.Page("src/Analytics.py", title="Analytics", icon=":material/addchart:")
 
+# Simulation results 
 results_page = st.Page("src/Results.py", title="Future Outlook", icon=":material/clock_arrow_up:")
 test_page = st.Page("src/sandbox.py", title="_SANDBOX_")
 
